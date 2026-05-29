d09a23c708fe24f5f7e06c0af8879d66762beba5
Caluusumus <carloluisotto@gmail.com>
2026-05-29 13:54:58 +0200

    Aggiunti Endpoints per la gestione delle API per Opere e Mostre

---
+++ MuseoCivicoAurora/MuseoCivicoAurora/MuseoCivicoAurora/Endpoints/MostreEndpoints.cs
@@ -0,0 +1,67 @@
+﻿using ClassModels;
+using Microsoft.AspNetCore.Http.HttpResults;
+using MuseoCivicoAurora.Service;
+
+namespace MuseoCivicoAurora.Endpoints;
+
+public static class MostreEndpoints
+{
+    public static IEndpointRouteBuilder MapMostreEndpoints(
+                                                this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("api/mostre");
+
+        group.MapGet("", GetAllAsync);
+        group.MapGet("{id:int}", GetByIdAsync);
+        group.MapPost("", AddAsync);
+        group.MapPut("{id:int}", UpdateAsync);
+        group.MapDelete("{id:int}", DeleteAsync);
+
+        return app;
+    }
+
+    private static async Task<Ok<IEnumerable<Mostra>>> GetAllAsync(MostreService service)
+    {
+        var mostre = await service.GetMostreAsync();
+
+        return TypedResults.Ok(mostre);
+    }
+
+    private static async Task<Results<NotFound, Ok<Mostra>>> GetByIdAsync(Guid id, MostreService service)
+    {
+        var mostra = await service.GetMostraByIdAsync(id);
+        if (mostra is null)
+            return TypedResults.NotFound();
+
+        return TypedResults.Ok(mostra);
+    }
+
+    private static async Task<Created<Mostra>> AddAsync(Mostra mostra, MostreService service)
+    {
+        await service.AddMostraAsync(mostra);
+
+        return TypedResults.Created($"/api/products/{mostra.Id}", mostra);
+    }
+
+    private static async Task<Results<NoContent, NotFound>> UpdateAsync(Guid id, Mostra mostra, MostreService service)
+    {
+        var found = await service.GetMostraByIdAsync(id);
+        if (found is null)
+            return TypedResults.NotFound();
+
+        await service.UpdateMostraAsync(mostra);
+
+        return TypedResults.NoContent();
+    }
+
+    private static async Task<Results<NoContent, NotFound>> DeleteAsync(Guid id, MostreService service)
+    {
+        var found = await service.GetMostraByIdAsync(id);
+        if (found is null)
+            return TypedResults.NotFound();
+
+        await service.DeleteMostraByIdAsync(id);
+
+        return TypedResults.NoContent();
+    }
+}
---
+++ MuseoCivicoAurora/MuseoCivicoAurora/MuseoCivicoAurora/Endpoints/OpereEndpoints.cs
@@ -0,0 +1,67 @@
+﻿using ClassModels;
+using Microsoft.AspNetCore.Http.HttpResults;
+using MuseoCivicoAurora.Service;
+
+namespace MuseoCivicoAurora.Endpoints;
+
+public static class OpereEndpoints
+{
+    public static IEndpointRouteBuilder MapOpereEndpoints(
+                                                this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("api/opere");
+
+        group.MapGet("", GetAllAsync);
+        group.MapGet("{id:int}", GetByIdAsync);
+        group.MapPost("", AddAsync);
+        group.MapPut("{id:int}", UpdateAsync);
+        group.MapDelete("{id:int}", DeleteAsync);
+
+        return app;
+    }
+
+    private static async Task<Ok<IEnumerable<Opera>>> GetAllAsync(OpereService service)
+    {
+        var opere = await service.GetOpereAsync();
+
+        return TypedResults.Ok(opere);
+    }
+
+    private static async Task<Results<NotFound, Ok<Opera>>> GetByIdAsync(Guid id, OpereService service)
+    {
+        var opera = await service.GetOperaByIdAsync(id);
+        if (opera is null)
+            return TypedResults.NotFound();
+
+        return TypedResults.Ok(opera);
+    }
+
+    private static async Task<Created<Opera>> AddAsync(Opera opera, OpereService service)
+    {
+        await service.AddOperaAsync(opera);
+
+        return TypedResults.Created($"/api/products/{opera.Id}", opera);
+    }
+
+    private static async Task<Results<NoContent, NotFound>> UpdateAsync(Guid id, Opera opera, OpereService service)
+    {
+        var found = await service.GetOperaByIdAsync(id);
+        if (found is null)
+            return TypedResults.NotFound();
+
+        await service.UpdateOperaAsync(opera);
+
+        return TypedResults.NoContent();
+    }
+
+    private static async Task<Results<NoContent, NotFound>> DeleteAsync(Guid id, OpereService service)
+    {
+        var found = await service.GetOperaByIdAsync(id);
+        if (found is null)
+            return TypedResults.NotFound();
+
+        await service.DeleteOperaByIdAsync(id);
+
+        return TypedResults.NoContent();
+    }
+}
