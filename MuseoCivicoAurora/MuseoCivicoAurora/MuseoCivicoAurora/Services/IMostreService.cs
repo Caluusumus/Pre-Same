985fcd283b92eaeabc954c0cb21b4776ca5c20f0
Caluusumus <carloluisotto@gmail.com>
2026-05-29 12:56:41 +0200

    Completata implementazione operazioni CRUD per opere e mostre con la generazione delle interfaccie

---
+++ MuseoCivicoAurora/MuseoCivicoAurora/MuseoCivicoAurora/Services/IMostreService.cs
@@ -0,0 +1,13 @@
+﻿using ClassModels;
+
+namespace MuseoCivicoAurora.Service
+{
+    public interface IMostreService
+    {
+        Task AddMostraAsync(Mostra mostra);
+        Task DeleteMostraByIdAsync(Guid id);
+        Task<Mostra?> GetMostraByIdAsync(Guid id);
+        Task<IEnumerable<Mostra>> GetMostreAsync();
+        Task UpdateMostraAsync(Mostra mostra);
+    }
+}
