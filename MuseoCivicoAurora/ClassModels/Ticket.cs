1a8275b7978df42df8618489b52c989b26428b03
GabrieleDistefano <antipulsaremail@gmail.com>
2026-06-01 10:54:05 +0200

    Frontend corretto, aggiunta micro-validazione

--- MuseoCivicoAurora/ClassModels/Ticket.cs
+++ MuseoCivicoAurora/ClassModels/Ticket.cs
@@ -4,6 +4,7 @@ public class Ticket
 {
     public Guid Id { get; set; }
     public string? FirstName { get; set; }
+    public string? LastName { get; set; }
     public string Email { get; set; } = default!;
     public string Type { get; set; } = default!;
     public int Quantity { get; set; }
